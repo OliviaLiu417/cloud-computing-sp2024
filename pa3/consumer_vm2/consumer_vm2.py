5202dd2929f7d7875a4a2d655b5d28afacc7c244
salomondush <63796975+salomondush@users.noreply.github.com>
2024-03-17 15:24:08 -0500

    Update Kafka bootstrap server and CouchDB URL

--- pa3/consumer_vm2/consumer_vm2.py
+++ pa3/consumer_vm2/consumer_vm2.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer  # consumer of events
 import json
-consumer = KafkaConsumer(bootstrap_servers="172.31.29.51:9092")
+consumer = KafkaConsumer(bootstrap_servers="kafka:9092")
 
 consumer.subscribe(topics=["weather_nashville", "weather_atlanta", "weather_chicago"])
 
